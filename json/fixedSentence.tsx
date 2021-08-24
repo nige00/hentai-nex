566f886624b894d8c0308dff363addb25857beac
nige00 <45668326+nige00@users.noreply.github.com>
2021-08-24 07:11:34 +0000

    contents end

---
+++ json/fixedSentence.tsx
@@ -0,0 +1,28 @@
+import "tailwindcss/tailwind.css";
+
+export const fiexdSentence = {
+  caution:
+    "本ページの情報は2021年8月時点のものです。最新の配信状況はU-NEXT公式サイトにてご確認ください。",
+  date: "【2021年8月】",
+  comment: (function () {
+    return (
+      <>
+        <p className="text-lg" style={{ paddingBottom: "16px" }}>
+          管理人より
+        </p>
+        <p style={{ paddingBottom: "8px" }}>
+          <span style={{ fontWeight: "bold" }}>マジで抜けます</span>
+          、これ見放題なのはヤバいです。
+        </p>
+        <p>ぜひ見ましょう、よかったらでいいので。</p>
+      </>
+    );
+  })(),
+  affiliateLink:
+    "https://lp.hnext.jp/browse/video/anime?utm_source=linka&utm_medium=a_n&utm_campaign=a_n_title_adult",
+  microCopy: "＼31日間無料 + 1200円分のポイントGET！／",
+  hnext: "H-NEXT",
+  hentai: "エロアニメ（HENTAI）",
+  keywords: "見放題,AV,H-NEXT,U-NEXT,エロアニメ,HENTAI,お得,30日間無料,VOD",
+  url: "https://hentai-nex.vercel.app",
+};
--- pages/paid.tsx
+++ pages/paid.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,39 +6,42 @@ import ReviewStar from "../components/ReviewStar";
 import { Datas } from "../json/Datas";
 import { fiexdSentence } from "../json/fixedSentence";
 
-const Home: NextPage = () => {
+const Allpages: NextPage = () => {
+  const paidDatas = Datas.filter((data) => {
+    return data.ondemand == "ポイント" || data.ondemand == "SALE";
+  });
+
   return (
     <Layout
-      title={`${fiexdSentence.date}H-NEXTでエロアニメが大量配信中｜人気作品をまとめました`}
-      description={`${fiexdSentence.hnext}で見放題の人気作品を毎月チェック！人気作品を一括で確認するのにおすすめです。`}
+      title={`${fiexdSentence.date}H-NEXTでPPV対象作品になっているエロアニメ作品一覧`}
+      description={`存在する全てのページを表示`}
       keyword={fiexdSentence.keywords}
-      url={`${fiexdSentence.url}/`}
-      type="website"
+      url={`${fiexdSentence.url}/posts`}
+      type="article"
     >
-      <h1 className="text-xl sm:text-2xl text-gray-600">
-        {`${fiexdSentence.date}H-NEXTでエロアニメが大量配信中｜人気作品をまとめました`}
-      </h1>
+      <h1 className="text-xl sm:text-2xl text-gray-600">{`${fiexdSentence.date}H-NEXTで見れる、PPVで配信されているエロアニメ作品【${paidDatas.length}件】`}</h1>
+      <div className="py-4 sm:px-4">
+        <p>{fiexdSentence.caution}</p>
+      </div>
       <div className="flex flex-col justify-center items-center py-4 cursor-pointer">
-        <span className="py-1 text-sm">＼H-NEXTとは？／</span>
+        <span className="py-1 text-sm">{fiexdSentence.microCopy}</span>
         <a
           className="py-3 px-8 text-xl text-white bg-pink-500 hover:bg-pink-400 rounded transition"
           href={fiexdSentence.affiliateLink}
           rel="nofollow noopener noreferrer"
           target="_blank"
         >
-          詳しくはこちらから
+          H-NEXTで見る
         </a>
       </div>
-      <div className="my-16 text-lg">{`現在のエロアニメ作品数：${Datas.length}本`}</div>
-      <h2 className="text-3xl">H-NEXTで見れるエロアニメランキングTOP100！</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-6 w-11/12">
-        {Datas.map((content, index) => {
+        {paidDatas.map((content, index) => {
           return (
             <div key={content.forUrlNumber}>
               <div className="flex flex-col justify-center items-center">
                 <span className="inline-block py-2 px-8 text-xl text-white bg-pink-400 rounded">{`${
                   index + 1
-                }位`}</span>
+                } / ${paidDatas.length}`}</span>
               </div>
               <div className="pt-4">
                 <Link href={`/posts/${content.forUrlNumber}`} passHref>
@@ -54,11 +56,7 @@ const Home: NextPage = () => {
                   </a>
                 </Link>
               </div>
-              <Link href={`/posts/${content.forUrlNumber}`} passHref>
-                <a>
-                  <h3 className="py-1 truncate">{content.title}</h3>
-                </a>
-              </Link>
+              <h3 className="py-1 truncate">{content.title}</h3>
               <ReviewStar star={content.aveReviewPoint} />
               {content.paid == "----" ? (
                 <p className="my-2">
@@ -78,19 +76,8 @@ const Home: NextPage = () => {
           );
         })}
       </div>
-      <div className="flex flex-col justify-center items-center py-4 cursor-pointer">
-        <span className="py-1 text-sm">{fiexdSentence.microCopy}</span>
-        <a
-          className="py-3 px-8 text-xl text-white bg-pink-500 hover:bg-pink-400 rounded transition"
-          href={fiexdSentence.affiliateLink}
-          rel="nofollow noopener noreferrer"
-          target="_blank"
-        >
-          エロアニメをもっと見る
-        </a>
-      </div>
     </Layout>
   );
 };
 
-export default Home;
+export default Allpages;
